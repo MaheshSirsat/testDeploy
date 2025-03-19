f7865b9959797ca348da5283cc070b9cdfff6d6c
MaheshSirsat <MaheshSirsat@ibm.com>
2025-03-19 09:38:20 +0530

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/portfolio/demo/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comportfolio/demo/test/unit/controller/View.controller"
+], function () {
+	"use strict";
+});
